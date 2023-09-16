82224a9f9bdb8212358c1ab7f8dc84b5ae0b6626
garytann <garytannjy@gmail.com>
2023-09-17 00:02:17 +0800

    added prefix for accounts,orders,menu routes

--- backend/api.py
+++ backend/api.py
@@ -3,6 +3,6 @@ from backend.endpoints import accounts, orders, menu
 
 api_router = APIRouter()
 
-api_router.include_router(accounts.router, tags=["Accounts"])
-api_router.include_router(orders.router, tags=["Orders"])
-api_router.include_router(menu.router, tags=["Menu"])
+api_router.include_router(accounts.router, tags=["Accounts"], prefix="/accounts")
+api_router.include_router(orders.router, tags=["Orders"], prefix="/orders")
+api_router.include_router(menu.router, tags=["Menu"], prefix="/menu")
