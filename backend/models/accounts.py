ca15cdfe51352800cdd91e0328fc24c3d7703bfa
garytann <garytannjy@gmail.com>
2023-09-18 16:23:58 +0800

    update accounts pydantic models to fix retrieval of records

--- backend/models/accounts.py
+++ backend/models/accounts.py
@@ -4,15 +4,15 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 class Accounts (BaseModel):
-    id: str = Field(default_factory=lambda: str(uuid.uuid4()), alias="_id")
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
     userid: str = Field(...)
     name: str = Field(...)
     contact: str = Field(...)
     address: str = Field(...)
 
     class Config:
-        allow_population_by_field_name = True
-        schema_extra = {
+        populate_by_name = True
+        json_schema_extra = {
             "example": {
                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
                 "userid": "192070187",
